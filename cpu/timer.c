a62d11dcef89af6f1f699cb68838da8f86e36e50
LeoDeSilva <leo.desilvafamily@gmail.com>
2023-10-28 02:39:20 +0900

    handle keyboard interrupts through driver

--- cpu/timer.c
+++ cpu/timer.c
@@ -17,12 +17,17 @@ void init_timer(u32 freq) {
     register_interrupt_handler(IRQ0, timer_callback);
 
     /* Get the PIT value: hardware clock at 1193180 Hz */
+    // The value we send to the PIT is the value to divide it's input clock
+    // (1193180 Hz) by, to get our required frequency. Important to note is
+    // that the divisor must be small enough to fit into 16-bits. 
     u32 divisor = 1193180 / freq;
     u8 low  = (u8)(divisor & 0xFF);
     u8 high = (u8)((divisor >> 8) & 0xFF);
 
     /* Send the command */
-    port_byte_out(0x43, 0x36); /* Command port */
+    port_byte_out(0x43, 0x36); /* PIT Command port */
+
+    // send the frequency divisor
     port_byte_out(0x40, low);
     port_byte_out(0x40, high);
 }
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -3,22 +3,12 @@
 #include "../cpu/isr.h"
 #include "../cpu/idt.h"
 #include "../cpu/timer.h"
+#include "../drivers/keyboard.h"
 
 void main() {
     isr_install();
-    asm volatile("sti");
-    init_timer(50);
+    asm volatile("sti"); // set interrupt flag (enable interrupts)
 
-    // clear_screen(WHITE_ON_BLACK);
-    // print_at("10 X = \"Hello World\"", 0, 0);
-    // print_at("20 PRINT X", 0, 1);
-    // print_at("30 GOTO 10", 0, 2);
-    // print("\n\n");
-
-    // for(int i = 0;; i++) {
-    //     char* out_str = iota(i);
-    //     print(out_str);
-    //     print(" Hello World\n");
-    //     for (int i = 0; i < 50000000; i++) {}
-    // }
+    // init_timer(50);
+    init_keyboard();
 }
